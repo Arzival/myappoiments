c308ea6a389d8d2dca2bce2c1a9142743c6742d7
Arzival <arzival.sistemas@gmail.com>
2021-06-15 22:50:51 -0500

    aplicando unas correciones de ruta

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,11 +11,11 @@
                         <div class="text-muted text-center mt-2 mb-3"><small>{{ __('Sign in with') }}</small></div>
                         <div class="btn-wrapper text-center">
                             <a href="#" class="btn btn-neutral btn-icon">
-                                <span class="btn-inner--icon"><img src="{{ asset('argon') }}/img/icons/common/github.svg"></span>
+                                <span class="btn-inner--icon"><img src="{{ asset('/img/icons/common/github.svg') }}"></span>
                                 <span class="btn-inner--text">{{ __('Github') }}</span>
                             </a>
                             <a href="#" class="btn btn-neutral btn-icon">
-                                <span class="btn-inner--icon"><img src="{{ asset('argon') }}/img/icons/common/google.svg"></span>
+                                <span class="btn-inner--icon"><img src="{{ asset('/img/icons/common/google.svg') }}"></span>
                                 <span class="btn-inner--text">{{ __('Google') }}</span>
                             </a>
                         </div>
