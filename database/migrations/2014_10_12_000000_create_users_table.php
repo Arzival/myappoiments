e36fcd3fe683cd6ad3dd578b75dd149acec6614e
Arzival <arzival.sistemas@gmail.com>
2021-06-25 22:09:31 -0500

    creando el Factory para usuarios

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,7 +21,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
 
-            $table->string('dni');
+            $table->string('dni')->nullable();
             $table->string('addres')->nullable();
             $table->string('phone')->nullable();
 
