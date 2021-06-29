a9dd4c6cbda09346aa2a059f93054eb6b2238535
Alberto Oficina <amengelle@preconcretoveracruz.com>
2021-06-29 18:47:25 -0500

    haciendo ajustes al nav lateral

--- resources/views/layouts/navbars/nav.blade.php
+++ resources/views/layouts/navbars/nav.blade.php
@@ -48,11 +48,6 @@
                             <i class="ni ni-circle-08 text-pink"></i> {{ __('Register') }}
                         </a>
                     </li>
-                    <li class="nav-item mb-5 mr-4 ml-4 pl-1 bg-danger" style="position: absolute; bottom: 0;">
-                        <a class="nav-link text-white" href="https://www.creative-tim.com/product/argon-dashboard-pro-laravel" target="_blank">
-                            <i class="ni ni-cloud-download-95"></i> Upgrade to PRO
-                        </a>
-                    </li>
                 </ul>
                 <!-- Divider -->
                 <hr class="my-3">
@@ -62,17 +57,12 @@
                 <ul class="navbar-nav mb-md-3">
                     <li class="nav-item">
                         <a class="nav-link" href="https://argon-dashboard-laravel.creative-tim.com/docs/getting-started/overview.html">
-                            <i class="ni ni-spaceship"></i> Getting started
+                            <i class="ni ni-spaceship"></i> Frecuencia de citas
                         </a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="https://argon-dashboard-laravel.creative-tim.com/docs/foundation/colors.html">
-                            <i class="ni ni-palette"></i> Foundation
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="https://argon-dashboard-laravel.creative-tim.com/docs/components/alerts.html">
-                            <i class="ni ni-ui-04"></i> Components
+                            <i class="ni ni-palette"></i> Medicos mas activos
                         </a>
                     </li>
                 </ul>
